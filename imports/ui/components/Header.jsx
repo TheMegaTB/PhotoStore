0a3b234807e9916a5e11bcc6f42a67a41ca91514
byYottaFLOPS <jonas.peeters@icloud.com>
2017-03-19 23:09:52 +0100

    Better title bar

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-import FlatButton from 'material-ui/FlatButton';
-
-import LoginButtons from './LoginButtons.jsx';
 
 export default class Header extends React.Component {
     render() {
         return (
             <header className='Header'>
                 <AppBar
-                    title={<span style={styles.title}>Title</span>}
-                    onTitleTouchTap={handleTouchTap}
-                    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-                    iconElementRight={<FlatButton label="Save" />}
+                    title={<span>Photo Store</span>}
+                    showMenuIconButton={false}
                 />
-
             </header>
         );
     }
 }
-
-function handleTouchTap() {
-    alert('onTouchTap triggered on the title component');
-}
-
-const styles = {
-    title: {
-        cursor: 'pointer',
-    },
-};
-
-
-const AppBarExampleIconButton = () => (
-    <AppBar
-        title={<span style={styles.title}>Title</span>}
-        onTitleTouchTap={handleTouchTap}
-        iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-        iconElementRight={<FlatButton label="Save" />}
-    />
-);
