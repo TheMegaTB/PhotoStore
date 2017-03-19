8af6762c1413ced52c7d90279a8fb5d09cd9c530
Til Blechschmidt <til@blechschmidt.de>
2017-03-19 21:44:13 +0100

    Added lazy load and image cards

--- client/main.js
+++ client/main.js
@@ -1,1 +1,7 @@
 import '../imports/startup/client/main.js';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
