b03555a2819e5933135768037ef4a86943b1cb80
Til Blechschmidt <til@blechschmidt.de>
2017-03-20 01:12:39 +0100

    Merge

--- imports/startup/server/main.js
+++ imports/startup/server/main.js
@@ -1,12 +1,9 @@
 
 console.log("Starting server...");
 
-<<<<<<< HEAD
 robots.addLine('User-agent: *');
 robots.addLine('Disallow: /');
 
-=======
->>>>>>> 9b47ccc5b80155a7ffe961c0a8dc5f8ccb8f1205
 import '../../api/api.js';
 
 const basicAuth = WebAppInternals.NpmModules.connect.module.basicAuth;
@@ -22,8 +19,4 @@ WebApp.connectHandlers.stack.splice(0, 0, {
     handle: basicAuth(function(user, pass) {
         return 'admin' == user && 'supersecret' == pass;
     })
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 9b47ccc5b80155a7ffe961c0a8dc5f8ccb8f1205
